906f6385a11babf657f3055c0a94c552ba42e845
“Martina <ma.covarrubias.a@gmail.com>
2017-07-10 15:55:43 -0400

    Pinté recetas en el div 'list-recipes' con imagenes e informacion requerida

--- js/script-index.js
+++ js/script-index.js
@@ -21,7 +21,7 @@ $(document).ready( function(){
 function renderHighlightedRecipes(recipesArray) {
 	recipesArray.forEach(function(el){
 		if(el.highlighted == true){
-			console.log(el);
+			console.log(el.highlighted);
 			renderRecipe(el);
 ;		}
 
@@ -29,7 +29,7 @@ function renderHighlightedRecipes(recipesArray) {
 
 	console.log('Recipes: ', recipesArray);
 
-}
+};
 
 
 /*
@@ -39,7 +39,19 @@ function renderHighlightedRecipes(recipesArray) {
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
+	$(".list-recipes").append('<a class="item-recipe" href="#">'+
+  '<span class="attribution">'+
+    '<span class="title-recipe">' + recipe.title + '</span>'+
+    '<span class="metadata-recipe">'+
+      '<span class="author-recipe">'+ recipe.source.name + '</span>'+
+      '<span class="bookmarks-recipe">'+
+        '<span class="icon-bookmark"></span>'+ 
+      '</span>'+
+    '</span>'+
+  '</span>'+
+'<img src="img/recipes/320x350/' + recipe.name + '.jpg"/>'+
+'</a>');
+	console.log('Voy a pintar la receta: ', recipe.title);
 }
 
 
