3e6c72ea15b21432553aa95726295719cf9f9b0a
“Martina <ma.covarrubias.a@gmail.com>
2017-07-11 00:37:37 -0400

    se agregan eventos click para navegacion en html recipes. Se da funcionalidad de volver a la pagina anterior al elemento arrow.

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,7 +3,17 @@ $(document).ready(function(){
  	console.log("El documento está listo :)")
 
  	$(".js-show-recipe").click(function(){
+ 		event.preventDefault();
  		$("#div-container").removeClass("make");
+ 		$(".js-show-recipe").addClass("active");
+ 		$(".js-show-make").removeClass("active");
+ 	});
+
+ 	$(".js-show-make").click(function(){
+ 		event.preventDefault();
+ 		$("#div-container").addclass("make");
+ 		$(".js-show-make").addclass("active");
+ 		$(".js-show-recipe").removeClass("active");
  	});
 
 });
